8cd9582de6ff798a2d64674fc21f82c4fa59cc57
Mark Drago <markdrago@gmail.com>
2012-02-04 09:45:58 -0500

    use require.js to load remote bookmarklet code
    this way the only dependency in the bookmarklet is require.js
    and if we ever have to be smart about how to load jquery those
    smarts can live on the remote side of the bookmarklet

--- js/cardorizer-creator.js
+++ js/cardorizer-creator.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
   $('#make-cardorizer').click(function() {
     $.ajax({
-      url:'/txt/cardorizer-local.txt',
+      url:'/data/bookmarklet.tmpl.txt',
       datatype: 'text',
       success: function(result) {
         var board_id = $('#board-id').val();
         var cardorizer_url = $('#cardorizer-url').val();
-        result = 'javascript:' + result.replace(/\s+/g, ' ').replace(/\n/g, ' ');
+        result = 'javascript:' + result.replace(/\s+/g, ' ').replace(/\n/g, '');
         result = result.replace(/\$\{board_id\}/g, board_id);
         result = result.replace(/\$\{cardorizer_url\}/g, cardorizer_url);
         $('#cardorizer-bookmarklet').html($('<a>').attr('href', result).append('Cardorizer'));
--- js/cardorizer-remote.js
+++ js/cardorizer-remote.js
@@ -1,5 +1,8 @@
+require(['js/jquery.min.js']);
+
 var key=document.getElementById("key-val").text;
 var summary=document.getElementById("issue_header_summary").children[0].text;
 var link=location.href;
 var card_name=key+": "+summary;
-var description=link;location=cardorizer_url+"?boardid="+board_id+"&name="+card_name+"&description="+description
+var description=link;
+location=cardorizer_url+"?boardid="+board_id+"&name="+card_name+"&description="+description
